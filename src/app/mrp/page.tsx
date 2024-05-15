bce889795033d0006682574c074c8f4284f68ea3
Zayne <star1582@naver.com>
2024-05-15 10:21:30 +0100

    feat: Add new pages for MRP, QA, Quotes, and Setting

---
+++ src/app/mrp/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">MRP</h1>
+      <p>This is a MRP page</p>
+    </div>
+  );
+};
+
+export default page;
