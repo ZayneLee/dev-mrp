c0c69504db12672ec81151d3c4b5dd461ebbf3f7
Zayne <star1582@naver.com>
2024-06-07 10:47:11 +0100

    setting

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+// src/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/cvc.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
