8571c5b5f3623c1d73276e46f292830b3e968077
Zayne <star1582@naver.com>
2024-06-11 10:13:34 +0100

    Add responsive calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Calendar from "@/components/Calendar";
+import Calendar from "@/components/Calendar/Calendar";
+import CalendarDev from "@/components/Calendar/CalendarDev";
 import Dashboard from "@/components/Dashboard";
 import Head from "next/head";
 
@@ -11,15 +12,9 @@ export default function Home() {
         <title>Main Page</title>
       </Head>
       <h1 className="text-3xl font-bold underline mb-4">Main Page</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="bg-white shadow-md rounded-lg p-4">
-          <h2 className="text-2xl font-bold mb-4">Calendar</h2>
-          <Calendar />
-        </div>
-        <div className="bg-white shadow-md rounded-lg p-4">
-          <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
-          <Dashboard />
-        </div>
+      <div className="bg-white shadow-md rounded-lg p-4">
+        <h2 className="font-bold text-2xl text-gray-700">Calendar</h2>
+        <CalendarDev />
       </div>
     </div>
   );
